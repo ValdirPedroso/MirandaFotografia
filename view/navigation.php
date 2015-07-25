2dd58bed2fae0a501af6a6900f6c0a38e9e4c85c
AndMorMat <andre_matos13@hotmail.com>
2015-07-25 00:15:24 -0300

    criando pasta view, e adicionando arquivos logo.php e navigation.php

---
+++ view/navigation.php
@@ -0,0 +1,14 @@
+<ul id="navigation">
+			<li class="active">
+				<a href="index.php">HOME</a>
+			</li>
+			<li>
+				<a href="about.php">SOBRE</a>
+			</li>
+			<li>
+				<a href="blog.php">TRABALHOS</a>
+			</li>
+			<li>
+				<a href="journey.php">CONTATO</a>
+			</li>
+</ul>
