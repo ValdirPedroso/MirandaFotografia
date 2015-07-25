f9e7e3a7eb06daa6fba2dac982fa45ffcfff51fe
AndMorMat <andre_matos13@hotmail.com>
2015-07-25 10:56:07 -0300

    Criando slide.php

---
+++ view/slide.php
@@ -0,0 +1,13 @@
+<figure id="figure">
+   <div class="trs">
+   		<span class="trs next"></span>
+   </div>
+   <div  class="trs">
+   		<span class="trs prev"></span>
+ 	</div>
+   <div id="slider">
+   	<a href="#" class="trs"><img  src="images/bg-featured-1.jpg"/></a>
+      <a href="#" class="trs"><img  src="images/bg-featured.jpg"/></a>
+   </div>
+   <figcaption></figcaption>
+</figure>
